b23acebbdb1c461b44dc6bb0c4362105f04df3f4
robdgs <107399627+robdgs@users.noreply.github.com>
2025-05-24 17:16:09 +0200

    exit with long long ✅

--- ft_exit.c
+++ ft_exit.c
@@ -6,47 +6,68 @@
 /*   By: rd-agost <rd-agost@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 17:38:22 by rd-agost          #+#    #+#             */
-/*   Updated: 2025/05/12 18:08:11 by rd-agost         ###   ########.fr       */
+/*   Updated: 2025/05/24 17:13:51 by rd-agost         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int ft_overflow(char *str) {
+	int i;
+	int len;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+') 
+		i++;
+	while (str[i] == '0') 
+		i++;
+	len = 0;
+	while (str[i + len] && ft_isdigit(str[i + len]))
+		len++;
+	if (len > 19) 
+		return 1;
+	if (len < 19) 
+		return 0;
+	if (str[0] == '-')
+		return (ft_strcmp(&str[i], "9223372036854775808") > 0);
+	return (ft_strcmp(&str[i], "9223372036854775807") > 0);
+}
+
 int ft_exit(char **cmd, t_ms *ms)
 {
-    int exit_code = ms->exit_status; // Default to last exit status
-    int i;
-    ft_putendl_fd("exit", STDOUT_FILENO);
-    
-    if (cmd[1] != NULL)
-    {
-        i = 0;
-        if (cmd[1][i] == '-' || cmd[1][i] == '+')
-            i++;
-        while (cmd[1][i])
-        {
-            if (!ft_isdigit(cmd[1][i]))
-            {
-                ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-                ft_putstr_fd(cmd[1], STDERR_FILENO);
-                ft_putendl_fd(": numeric argument required", STDERR_FILENO);
-				//ft_error_print(cmd[1], ms, 2);al posto delle 3 linee sopra
-                ms_cleanup(ms);
-                exit(2);
-            }
-            i++;
-        }
+	int exit_code = ms->exit_status;
+	int i;
+	ft_putendl_fd("exit", STDOUT_FILENO);
 
-        if (cmd[2] != NULL)
-        {
-            ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
-            ms->exit_status = 1;
-            return 1; 
-        }
-        exit_code = ft_atoi(cmd[1]) & 255;
-    }
+	if (cmd[1] != NULL)
+	{
+		i = 0;
+		if (cmd[1][i] == '-' || cmd[1][i] == '+')
+			i++;
+		while (cmd[1][i])
+		{
+			if (!ft_isdigit(cmd[1][i]) || ft_overflow(cmd[1]))
+			{
+				ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+				ft_putstr_fd(cmd[1], STDERR_FILENO);
+				ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+				//ft_error_print(cmd[1], ms, 2);al posto delle 3 linee sopra
+				ms_cleanup(ms);
+				exit(2);
+			}
+			 i++;
+		}
+
+		if (cmd[2] != NULL)
+		{
+			ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+			ms->exit_status = 1;
+			return 1; 
+		}
+		exit_code = ft_atoi(cmd[1]) & 255;
+	}
 	ft_clear_cmdblock(&ms->cmdblocks);
-    ms_cleanup(ms);
-    exit(exit_code & 255);
-    return 42; // Never reached
+	ms_cleanup(ms);
+	exit(exit_code & 255);
+	return 42; // Never reached
 }
